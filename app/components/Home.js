eb3a6a08d75788b85d309eb36818a4985e914cec
Pavel Topinka <ptopinka@gmail.com>
2015-12-18 16:51:43 +0100

    first commit

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Home = React.createClass({
+
+    render: function() {
+        return(
+            <h2 className="text-center">
+                Search Git
+            </h2>
+        )
+    }
+})
+
+module.exports = Home;
